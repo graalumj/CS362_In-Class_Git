51aa6f732c45e79631c048a4ae59687c4f5295c6
AlexGraalum <30445154+AlexGraalum@users.noreply.github.com>
2021-04-15 16:56:33 -0700

    Update Calculator.py
    Subtract, Multiply, and Divide added to program

--- src/Calculator.py
+++ src/Calculator.py
@@ -5,3 +5,41 @@
 
 def add(x, y):
     return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+    
+print("1) Add")
+print("2) Subtract")
+print("3) Multiply")
+print("4) Divide")
+
+while True:
+    operate = input("Enter Operation: ")
+    
+    if operate in (1, 2, 3, 4):
+        numA = float(input("Input 1: "))
+        numB = float(input("Input 2: "))
+        
+        if operate == 1:
+            string = str(numA) + " + " + str(numB) + " = " + str(add(numA, numB))
+            print(string)
+        if operate == 2:
+            string = str(numA) + " - " + str(numB) + " = " + str(sub(numA, numB))
+            print(string)
+        if operate == 3:
+            string = str(numA) + " * " + str(numB) + " = " + str(mul(numA, numB))
+            print(string)
+        if operate == 4:
+            string = str(numA) + " / " + str(numB) + " = " + str(div(numA, numB))
+            print(string)
+        break
+
+    else:
+        print("ERROR: Input not valid")
