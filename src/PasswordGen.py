8a5d28072bb37b772330288be6d0f3d3b0d36db8
AlexGraalum <graalumj@oregonstate.edu>
2021-04-16 14:07:00 -0700

    PasswordGen complete
    Passwordgen.py complete with random generation of ASCII letters (upper- and lowercase) and numbers

--- src/PasswordGen.py
+++ src/PasswordGen.py
@@ -2,7 +2,9 @@
 #Alex Graalum
 #CS 362
 #4-15-2021
+import random
+import string
+characters = string.ascii_lowercase + string.ascii_uppercase + string.digits 
 
 num = input("Enter password length: ")
-print("Your password is: ")
-
+print("Your password is: " + ''.join(random.choice(characters) for i in range(num)))
