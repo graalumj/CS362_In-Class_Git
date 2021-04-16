a2dbd4ab7625a38bda3cd5b56d9c51d393b3e049
AlexGraalum <30445154+AlexGraalum@users.noreply.github.com>
2021-04-15 17:00:47 -0700

    Calculator I/O
    User input and information output in Calculator.py has been defined and is functional

--- src/Calculator.py
+++ src/Calculator.py
@@ -14,3 +14,32 @@ def mul(x, y):
 
 def div(x, y):
     return x / y
+    
+print("1) Add")
+print("2) Subtract")
+print("3) Multiply")
+print("4) Divide")
+
+while True:
+    operate = input("Enter Operation: ")
+    
+    if operate in (1, 2, 3, 4):
+        numA = float(input("Input 1: "))
+        numB = float(input("Input 2: "))
+        
+        if operate == 1:
+            string = str(numA) + " + " + str(numB) + " = " + str(add(numA, numB))
+            print(string)
+        if operate == 2:
+            string = str(numA) + " - " + str(numB) + " = " + str(sub(numA, numB))
+            print(string)
+        if operate == 3:
+            string = str(numA) + " * " + str(numB) + " = " + str(mul(numA, numB))
+            print(string)
+        if operate == 4:
+            string = str(numA) + " / " + str(numB) + " = " + str(div(numA, numB))
+            print(string)
+        break
+
+    else:
+        print("ERROR: Input not valid")
